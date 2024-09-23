fdc8adbcac44ad26b5f44c81a3dc4f7fabad610f
Grenish rai <107925840+Grenish@users.noreply.github.com>
2024-09-23 12:06:54 +0530

    chore: fixed server end point

--- client/app/chat/page.tsx
+++ client/app/chat/page.tsx
@@ -36,7 +36,7 @@ const ChatComponent = () => {
   const handleBotResponse = async (userMessage: string) => {
     setIsLoading(true);
     try {
-      const response = await axios.post("https://aayu-cljk.onrender.com/", {
+      const response = await axios.post("https://aayu-cljk.onrender.com/api/generate", {
         input: userMessage,
       });
 
